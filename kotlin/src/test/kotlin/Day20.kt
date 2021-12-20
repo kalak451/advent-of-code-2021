dc51639f156be766712370c3ca46326c4ac1984c
Travis Klotz <travis.klotz@gmail.com>
2021-12-20 11:29:08 -0600

    day 20 cleanup

--- kotlin/src/test/kotlin/Day20.kt
+++ kotlin/src/test/kotlin/Day20.kt
@@ -6,64 +6,53 @@ class Day20 {
 
     @Test
     fun shouldExtreme() {
-        val (_, alg) = loadData("aoc-2021-20.txt")
-        val image = (0..99).map {
+        val orig = loadImage("aoc-2021-20.txt")
+        val data = (0..99).map {
             (0..99).map { '.' }.joinToString("")
         }
 
-        val enhanced1 = enhance(image, alg, '.')
-        val enhanced2 = enhance(enhanced1, alg, alg[0])
-        val enhanced3 = enhance(enhanced2, alg, alg[511])
+        val image = Image(orig.alg, data)
 
-        assertEquals(0, enhanced2.joinToString("").count { it == '#' })
+        val enhanced1 = image.enhance()
+        val enhanced2 = enhanced1.enhance()
+        val enhanced3 = enhanced2.enhance()
+
+        assertEquals(0, enhanced2.data.joinToString("").count { it == '#' })
+        assertEquals(0, enhanced3.data.joinToString("").count { it == '.' })
     }
 
     @Test
     fun part1() {
-        val (testImage, testAlg) = loadData("aoc-2021-20-test.txt")
-        val testEnhanced1 = enhance(testImage, testAlg, '.')
-        val testEnhanced2 = enhance(testEnhanced1, testAlg, testAlg[0])
+        val testImage = loadImage("aoc-2021-20-test.txt")
+        val testEnhanced1 = testImage.enhance()
+        val testEnhanced2 = testEnhanced1.enhance()
 
-        val testCnt = testEnhanced2.joinToString("").count { it == '#' }
+        val testCnt = testEnhanced2.data.joinToString("").count { it == '#' }
         assertEquals(35, testCnt)
 
-        val (image, alg) = loadData("aoc-2021-20.txt")
+        val image = loadImage("aoc-2021-20.txt")
 
-        val enhanced1 = enhance(image, alg, '.')
-        val enhanced2 = enhance(enhanced1, alg, alg[0])
+        val enhanced1 = image.enhance()
+        val enhanced2 = enhanced1.enhance()
 
-        val cnt = enhanced2.joinToString("").count { it == '#' }
+        val cnt = enhanced2.data.joinToString("").count { it == '#' }
         assertEquals(5291, cnt)
     }
 
     @Test
     fun part2() {
-        val (testImage, testAlg) = loadData("aoc-2021-20-test.txt")
+        val testImage = loadImage("aoc-2021-20-test.txt")
 
-        val (_,testEnhanced) = generateSequence(Pair(1, enhance(testImage, testAlg, '.'))) {
-            val (flag, acc) = it
-            if(flag == 1) {
-                Pair(0, enhance(acc, testAlg, testAlg[0]))
-            } else {
-                Pair(1, enhance(acc, testAlg, testAlg[0]))
-            }
-        }.take(50).last()
+        val testEnhanced = generateSequence(testImage.enhance()) { it.enhance() }.take(50).last()
 
-        val testCnt = testEnhanced.joinToString("").count { it == '#' }
+        val testCnt = testEnhanced.data.joinToString("").count { it == '#' }
         assertEquals(3351, testCnt)
 
-        val (image, alg) = loadData("aoc-2021-20.txt")
+        val image = loadImage("aoc-2021-20.txt")
 
-        val (_,enhanced) = generateSequence(Pair(1, enhance(image, alg, '.'))) {
-            val (flag, acc) = it
-            if(flag == 1) {
-                Pair(0, enhance(acc, alg, alg[0]))
-            } else {
-                Pair(1, enhance(acc, alg, alg[511]))
-            }
-        }.take(50).last()
+        val enhanced = generateSequence(image.enhance()) { it.enhance() }.take(50).last()
 
-        val cnt = enhanced.joinToString("").count { it == '#' }
+        val cnt = enhanced.data.joinToString("").count { it == '#' }
         assertEquals(16665, cnt)
     }
 
@@ -91,83 +80,17 @@ class Day20 {
             "....###..",
         )
 
-        val (testImage, testAlg) = loadData("aoc-2021-20-test.txt")
+        val testImage = loadImage("aoc-2021-20-test.txt")
 
 
-        val testEnhanced1 = enhance(testImage, testAlg, '.')
-        val testEnhanced2 = enhance(testEnhanced1, testAlg, testAlg[0])
+        val testEnhanced1 = testImage.enhance()
+        val testEnhanced2 = testEnhanced1.enhance()
 
-        assertEquals(expected1, testEnhanced1)
-        assertEquals(expected2, testEnhanced2)
+        assertEquals(expected1, testEnhanced1.data)
+        assertEquals(expected2, testEnhanced2.data)
     }
 
-    fun enhance(input: List<String>, alg: String, expansionChar: Char): List<String> {
-        val image = expandImage(input, expansionChar)
-        return image.mapIndexed { y, row ->
-            row.indices.map { x ->
-                val idx = buildAdj(listOf(x, y))
-                    .map { pt -> image.at(pt, expansionChar) }
-                    .map { c ->
-                        when (c) {
-                            '.' -> '0'
-                            '#' -> '1'
-                            else -> throw RuntimeException("Invalid char $c")
-                        }
-                    }
-                    .joinToString("")
-                    .toInt(2)
-
-                alg[idx]
-            }.joinToString("")
-        }
-    }
-
-    fun List<String>.at(pt: List<Int>, expansionChar: Char): Char {
-        if (pt.all { it in 0 until this.size }) {
-            return this[pt[1]][pt[0]]
-        }
-
-        return expansionChar
-    }
-
-    fun buildAdj(pt: List<Int>): List<List<Int>> {
-        return ((pt[1] - 1)..(pt[1] + 1)).flatMap { y ->
-            ((pt[0] - 1)..(pt[0] + 1)).map { x ->
-                listOf(x, y)
-            }
-        }
-    }
-
-    @Test
-    fun shouldExpandImage() {
-        val input = listOf(
-            "#..#.",
-            "#....",
-            "##..#",
-            "..#..",
-            "..###",
-        )
-
-        val expected = listOf(
-            ".......",
-            ".#..#..",
-            ".#.....",
-            ".##..#.",
-            "...#...",
-            "...###.",
-            "......."
-        )
-
-        assertEquals(expected, expandImage(input, '.'))
-    }
-
-    fun expandImage(input: List<String>, expansionChar: Char): List<String> {
-        val emptyRow = arrayOfNulls<Char>(input[0].length).map { expansionChar }.joinToString("")
-
-        return (listOf(emptyRow) + input + listOf(emptyRow)).map { row -> "$expansionChar$row$expansionChar" }
-    }
-
-    fun loadData(path: String): Pair<List<String>, String> {
+    fun loadImage(path: String): Image {
         val lines = File(ClassLoader.getSystemResource(path).file).readLines()
 
         val alg = lines.take(1).first()
@@ -176,6 +99,54 @@ class Day20 {
 
         assertEquals(512, alg.length)
 
-        return Pair(image, alg)
+        return Image(alg, image)
+    }
+
+
+    class Image(val alg: String, val data: List<String>, private val unknown: Char = '.') {
+        fun enhance(): Image {
+            val newData = (-1..data.size).map { y ->
+                (-1..data.size).map { x ->
+                    val idx = buildAdj(listOf(x, y)).map { pt -> at(pt) }.map { c ->
+                            when (c) {
+                                '.' -> '0'
+                                '#' -> '1'
+                                else -> throw RuntimeException("Invalid char $c")
+                            }
+                        }.joinToString("").toInt(2)
+                    alg[idx]
+                }.joinToString("")
+            }
+
+            val nextUnknown = calculateNextUnknown()
+
+            return Image(alg, newData, nextUnknown)
+        }
+
+        private fun buildAdj(pt: List<Int>): List<List<Int>> {
+            return ((pt[1] - 1)..(pt[1] + 1)).flatMap { y ->
+                ((pt[0] - 1)..(pt[0] + 1)).map { x ->
+                    listOf(x, y)
+                }
+            }
+        }
+
+        private fun calculateNextUnknown(): Char {
+            return if (unknown == '.' && alg[0] == '.') {
+                '.'
+            } else if (unknown == alg[0]) {
+                alg[1]
+            } else {
+                alg[0]
+            }
+        }
+
+        private fun at(pt: List<Int>): Char {
+            if (pt.all { it in data.indices }) {
+                return data[pt[1]][pt[0]]
+            }
+
+            return unknown
+        }
     }
 }
